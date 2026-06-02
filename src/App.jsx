94774a393877d3ee6432152c36daff1c8e142d8c
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-06-02 18:16:26 +0530

    'skfd'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+import Section from "./components/Section"
+import './components/styles.css'
+function App(){
+  return (
+   <div>
+    <Navbar/>
+    <Section/>
+   </div>
+  )
+}
+export default App
