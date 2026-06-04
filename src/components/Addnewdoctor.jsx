a9f8ee4cd0a2e42e61d12a91c1dd25a96cb2138f
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-06-04 19:28:50 +0530

    cvv

--- src/components/Addnewdoctor.jsx
+++ src/components/Addnewdoctor.jsx
@@ -6,14 +6,23 @@ function Addnewdoctor() {
   const [age,setAge]=useState('')
   const [salary,setSalary]=useState('')
   const [specialization,setSpecialization]=useState('')
+
+  function handlesubmit(e){
+    e.preventDefault()
+    let newdoctor={
+      name,age,gender,salary,specialization
+    }
+    console.log(newdoctor)
+  }
+  
   return (
     <div >
       <h1>Add New Doctor</h1>
-      <form action="" className='form-container'>
+      <form action="" className='form-container' onSubmit={handlesubmit}>
         <input value={name} onChange={(e)=>setName(e.target.value)} type="text" placeholder='Enter Doctor Name' className='text-field'/>
         <input value={age} onChange={(e)=>setAge(e.target.value)} type="text" placeholder='Enter Age' className='text-field'/>
         
-        <select value={gender} onChane={(e)=>setGender(e.target.value)} name="" id="">
+        <select value={gender} onChange={(e)=>setGender(e.target.value)} name="" id="">
           <option >Select Gender</option>
           <option value='Male'> Male</option>
           <option value='Female'> Female</option>
@@ -21,7 +30,7 @@ function Addnewdoctor() {
 
         <input value={specialization} onChange={(e)=>setSpecialization(e.target.value)} type="text" placeholder='Enter Specialization' className='text-field'/>
         <input value={salary} type="text" onChange={(e)=>setSalary(e.target.value)} placeholder='Enter Salary' className='text-field'/>
-        <button > Add Doctor</button>
+        <button  type='submit' > Add Doctor</button>
       </form>
     </div>
   )
